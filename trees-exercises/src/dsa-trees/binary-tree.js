3f7d9bf5c9a6ff14a26a872071cf6c81c870e749
Alexandra Colorado <alexandrapena19@gmail.com>
2024-03-15 23:08:44 -0400

    Update

---
+++ trees-exercises/src/dsa-trees/binary-tree.js
@@ -0,0 +1,162 @@
+/** BinaryTreeNode: node for a general tree. */
+
+class BinaryTreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** minDepth(): return the minimum depth of the tree -- that is,
+   * the length of the shortest path from the root to a leaf. */
+
+  minDepth() {
+    if (!this.root) return 0; 
+
+    const minDepthHelper = (node) => {
+      if (node.right === null && node.left === null) return 1;
+
+      if(node.left === null ) return minDepthHelper(node.right)+1;
+      if(node.right === null) return minDepthHelper(node.left) +1;
+      return (
+        Math.min(minDepthHelper(node.left), minDepthHelper(node.right))+1
+      );
+    }
+    return minDepthHelper(this.root);
+
+  }
+
+  /** maxDepth(): return the maximum depth of the tree -- that is,
+   * the length of the longest path from the root to a leaf. */
+
+  maxDepth() {
+    if (!this.root) return 0; 
+
+    const maxDepthHelper = (node) => {
+      if (node.left === null && node.right === null) return 1;
+      if(node.left === null ) return maxDepthHelper(node.right)+1;
+      if(node.right === null) return maxDepthHelper(node.left) +1;
+      return (
+        Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right))+1
+      );
+    }
+    return maxDepthHelper(this.root);
+
+  }
+
+  /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
+   * The path doesn't need to start at the root, but you can't visit a node more than once. */
+
+  maxSum() {
+    if (!this.root) return 0; 
+    let results = 0;
+
+    const maxSumHelper = (node) => {
+      //If there are no nodes, return 0
+      if (node === null) return 0;
+      //Pass node.left as param for maxSumHelper
+       
+      const leftSum = maxSumHelper(node.left);
+      const rightSum = maxSumHelper(node.right);
+      results = Math.max(results, node.val + leftSum + rightSum);
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+
+    }
+    maxSumHelper(this.root);
+    return results;
+  }
+
+  /** nextLarger(lowerBound): return the smallest value in the tree
+   * which is larger than lowerBound. Return null if no such value exists. */
+
+  nextLarger(lowerBound) {
+    if (!this.root) return null; 
+
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length){
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let highweThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+      if(highweThanLowerBound && shouldReassignClosest){
+        closest = currentVal;
+  
+      }
+      if (currentNode.left) queue.push(currentNode.left);
+      if(currentNode.right) queue.push(currentNode.right);
+
+    }
+    return closest;
+
+  }
+
+  /** Further study!
+   * areCousins(node1, node2): determine whether two nodes are cousins
+   * (i.e. are at the same level but have different parents. ) */
+
+  areCousins(node1, node2) {
+    if (node1 === this.root || node2 === this.root) return false;
+
+    function findLevelAndParent(
+      nodeToFind,
+      currentNode,
+      level = 0,
+      data = { level: 0, parent: null }
+    ) {
+      if (data.parent) return data;
+      if (currentNode.left === nodeToFind || currentNode.right === nodeToFind) {
+        data.level = level + 1;
+        data.parent = currentNode;
+      }
+      if (currentNode.left) {
+        findLevelAndParent(nodeToFind, currentNode.left, level + 1, data);
+      }
+      if (currentNode.right) {
+        findLevelAndParent(nodeToFind, currentNode.right, level + 1, data);
+      }
+      return data;
+    }
+
+    let node1Info = findLevelAndParent(node1, this.root);
+    let node2Info = findLevelAndParent(node2, this.root);
+
+    let sameLevel =
+      node1Info && node2Info && node1Info.level === node2Info.level;
+    let differentParents =
+      node1Info && node2Info && node1Info.parent !== node2Info.parent;
+    return sameLevel && differentParents;
+  
+  }
+
+  /** Further study!
+   * serialize(tree): serialize the BinaryTree object tree into a string. */
+
+  static serialize() {
+
+  }
+
+  /** Further study!
+   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
+
+  static deserialize() {
+
+  }
+
+  /** Further study!
+   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
+   * of two nodes in a binary tree. */
+
+  lowestCommonAncestor(node1, node2) {
+    
+  }
+}
+
+module.exports = { BinaryTree, BinaryTreeNode };
